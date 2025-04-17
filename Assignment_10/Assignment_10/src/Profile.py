25d4a53841e58cb92644d92375dbafeff0368fd9
KutlwanoSetungwane01 <setungwanekutlwano@gmail.com>
2025-04-17 10:04:33 +0200

    Profile.py

---
+++ Assignment_10/Assignment_10/src/Profile.py
@@ -0,0 +1,6 @@
+class Profile:
+    def __init__(self, profile_id, skills, portfolio, rating):
+        self.profile_id = profile_id
+        self.skills = skills
+        self.portfolio = portfolio
+        self.rating = rating
