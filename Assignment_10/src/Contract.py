e9f094466c05968a4efcf5bfac9c35fefb97cc5e
KutlwanoSetungwane01 <setungwanekutlwano@gmail.com>
2025-04-21 08:36:37 +0200

    Contract.py

---
+++ Assignment_10/src/Contract.py
@@ -0,0 +1,6 @@
+class Contract:
+    def __init__(self, contract_id, start_date, end_date, terms):
+        self.contract_id = contract_id
+        self.start_date = start_date
+        self.end_date = end_date
+        self.terms = terms
