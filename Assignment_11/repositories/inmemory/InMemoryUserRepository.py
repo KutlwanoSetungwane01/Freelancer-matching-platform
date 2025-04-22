d4ded1a0a291f6bce37ee7a0b589e6d9166f7fe9
KutlwanoSetungwane01 <setungwanekutlwano@gmail.com>
2025-04-22 12:29:17 +0200

    Update InMemoryUserRepository.py

--- Assignment_11/repositories/inmemory/InMemoryUserRepository.py
+++ Assignment_11/repositories/inmemory/InMemoryUserRepository.py
@@ -1,1 +1,19 @@
+from Assignment_11.repositories.UserRepository import UserRepository
+from Assignment_10.src.User import User
 
+class InMemoryUserRepository(UserRepository):
+    def __init__(self):
+        self._storage = {}
+
+    def save(self, user: User) -> None:
+        self._storage[user.user_id] = user
+
+    def find_by_id(self, user_id: str) -> User:
+        return self._storage.get(user_id)
+
+    def find_all(self):
+        return list(self._storage.values())
+
+    def delete(self, user_id: str) -> None:
+        if user_id in self._storage:
+            del self._storage[user_id]
