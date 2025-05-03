2d5cc58ad0440f8ecda24a30d824c5a0260fe26f
KutlwanoSetungwane01 <setungwanekutlwano@gmail.com>
2025-05-03 17:37:41 +0200

    Update main.py

--- Assignment_12/main.py
+++ Assignment_12/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from services import jobpost_routes, proposal_routes
 
 app = FastAPI()
 
+# Root endpoint
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI!"}
 
+# Include your routers
+app.include_router(jobpost_routes.router, prefix="/jobs", tags=["Job Posts"])
+app.include_router(proposal_routes.router, prefix="/proposals", tags=["Proposals"])
