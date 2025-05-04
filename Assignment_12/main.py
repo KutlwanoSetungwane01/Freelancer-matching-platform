ef0abd616cf5f2bff1f17aba81f2d439329776f8
KutlwanoSetungwane01 <setungwanekutlwano@gmail.com>
2025-05-04 17:57:24 +0200

    Update main.py

--- Assignment_12/main.py
+++ Assignment_12/main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
-from services import jobpost_routes, proposal_routes
+from services import jobpost_routes, proposal_routes, contract_routes
 
 app = FastAPI(title="Freelancer Job Matching Platform API")
 
-# Root endpoint
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI!"}
 
-# Register routers
 app.include_router(jobpost_routes.router, prefix="/jobs", tags=["Job Posts"])
 app.include_router(proposal_routes.router, prefix="/proposals", tags=["Proposals"])
+app.include_router(contract_routes.router, prefix="/contracts", tags=["Contracts"])
